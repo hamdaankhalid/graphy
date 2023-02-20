5f71d69baea6ee96acc80fcf8b0bf2257650f1d6
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2023-02-20 14:06:03 -0800

    init graph comp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,42 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
-import Card from 'components/card'
-import { WhatContent, WhyContent, HowContent } from 'constants/landingPage'
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import Card from "components/card";
+import { WhatContent, WhyContent, HowContent } from "constants/landingPage";
 
 export default function Home() {
   return (
     <>
-			<Head>
-			<title>Graphy</title>
-				<meta name="description" content="Graph your data" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-			</Head>
+      <Head>
+        <title>Graphy</title>
+        <meta name="description" content="Graph your data" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
 
-			<div className="cards"> 
-				<Card key={1} cardHeader="What?" cardInfo={WhatContent.slice(0, 60) + "..."} showModal={true} modalText={WhatContent} />
+      <div className="cards">
+        <Card
+          key={1}
+          cardHeader="What?"
+          cardInfo={WhatContent.slice(0, 60) + "..."}
+          showModal={true}
+          modalText={WhatContent}
+        />
 
-				<Card key={2} cardHeader="Why?" cardInfo={WhyContent.slice(0, 60) + "..."} showModal={false} modalText={WhyContent}/>
-				
-				<Card key={3} cardHeader="How?"	cardInfo={HowContent.slice(0, 60) + "..."} showModal={false} modalText={HowContent}/>
-			</div>			
+        <Card
+          key={2}
+          cardHeader="Why?"
+          cardInfo={WhyContent.slice(0, 60) + "..."}
+          showModal={false}
+          modalText={WhyContent}
+        />
+
+        <Card
+          key={3}
+          cardHeader="How?"
+          cardInfo={HowContent.slice(0, 60) + "..."}
+          showModal={false}
+          modalText={HowContent}
+        />
+      </div>
     </>
-  )
+  );
 }
-
