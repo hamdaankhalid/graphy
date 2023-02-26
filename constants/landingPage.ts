246e1563d7733dca79c72ce19f57c64b569f6dbd
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2023-02-25 18:27:19 -0800

    fix typo

--- constants/landingPage.ts
+++ constants/landingPage.ts
@@ -1,5 +1,4 @@
-
-export const WhatContent = `Graphy is a small project created to facilitate managenement, visualization, and exploration of different foramts of data.
+export const WhatContent = `Graphy is a small project created to facilitate managenement, visualization, and exploration of different formats of data.
 	Initially I only intend to support field delimitted files such as csv. We hope to expand to other file formats time permitted.`;
 
 export const WhyContent = `I work as a full-time backend engineer. The closest interaction I have with UI is ClI's and Postman. I truly believe, that
@@ -8,4 +7,3 @@ export const WhyContent = `I work as a full-time backend engineer. The closest i
 
 export const HowContent = `The frontend is built using NextJs, CSS, and Typescript. I intend to leverage my already existing Ruby on Rails server, and even use
 	NextJs's server side rendering actions for certain features. For calculation intensive procideures I will be looking at building my own little Rust service.`;
-
