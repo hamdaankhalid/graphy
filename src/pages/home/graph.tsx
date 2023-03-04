8c545a0b7ec00f1fca533132716086ae119f447c
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2023-03-03 22:39:12 -0800

    make graphy more awesome

--- src/pages/home/graph.tsx
+++ src/pages/home/graph.tsx
@@ -2,8 +2,16 @@ import LineGraph from "components/lineGraph";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0/client";
 
 function Graph() {
+  // This data will be picked from Upload data bar component by the csv file selected
+  // When this data is picked we will fire an event that our current Graph will correspond to
   const x = Array.from({ length: 100 }, (_, i) => i + 1);
-  const y = [x, x.map((i) => i ** 2), x.map((i) => i ** 0.5)];
+  const y = [
+    x,
+    x.map((i) => i ** 2),
+    x.map((i) => i ** 0.5),
+    x.map((i) => 9 + Math.sin(i)),
+  ];
+
   return (
     <>
       <div className="graphPage">
